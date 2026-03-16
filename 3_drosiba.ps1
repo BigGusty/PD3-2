672b42858470c50052999d46b6a7fceab6cf611f
BigGusty <a250626gt@rvt.lv>
2026-03-16 14:57:36 +0200

    Add files via upload

---
+++ 3_drosiba.ps1
@@ -0,0 +1,17 @@
+$reitings = 100
+
+$status = Get-MpComputerStatus
+
+if($status.RealTimeProtectionEnabled -eq $false) {
+	$reitings -= 50
+}
+
+if($status.AntivirusSignatureAge -gt 3) {
+	$reitings -= 20
+}
+
+if($status.QuickScanAge -gt 7) {
+	$reitings -= 20
+}
+
+Write-Host "Sistemas drosibas reitings: $reitings / 100"
