b678dd160f96550d5de612d2ea0852c3f7e47d27
BigGusty <a250626gt@rvt.lv>
2026-03-16 13:56:21 +0200

    Add files via upload

---
+++ 1_procesi.ps1
@@ -0,0 +1,12 @@
+$limits = 150MB
+
+$failaCels = "$env:USERPROFILE/Documents/LielieProcesi.csv"
+
+Get-Process |
+Where-Object {
+	$_.WorkingSet -gt $limits -and $_.Name -ne "msedge"
+} |
+Select-Object Name, Id, WorkingSet |
+Export-Csv -Path $failacels -NoTypeInformation -Encoding UTF8
+
+Write-Host "Procesu saraksts saglabāts $failaCels"
